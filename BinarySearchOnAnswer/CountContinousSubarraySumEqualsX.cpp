47670cfebf11f593c4a3729e2ff721480009df76
vhoanghviet <dhoangviet3005@gmail.com>
2026-03-24 21:01:02 +0700

    Stop code at 09:00PM 24/03/2026

---
+++ BinarySearchOnAnswer/CountContinousSubarraySumEqualsX.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using ll = long long;
+using namespace std;
+
+// Do bài này sử dụng map -> mình sẽ skip và học lại map trước khi làm
+
+void solve()
+{
+    int n, x;
+    cin >> n >> x;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    ll tong = 0;
+    map<ll, int> mp;
+    for(int i = 0; i < n; i++) {
+
+    }
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    solve();
+    return 0;
+}
